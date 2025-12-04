53046a13b6712513489e420f3d52d848a1e695d9
FRS <fabian.saladino3@gmail.com>
2025-12-04 15:24:10 -0300

    Delete alumnos directory

--- alumnos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Alumno
-
-@admin.register(Alumno)
-class AlumnoAdmin(admin.ModelAdmin):
-    list_display = ('ID_Legajo', 'nombre', 'apellido', 'nivel', 'tutor')
--- alumnos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('alta/', views.alta_alumno, name='alta_alumno'),
-    path('buscar/', views.buscar_alumno, name='buscar_alumno'),
-    path('detalle/<int:legajo>/', views.detalle_alumno, name='detalle_alumno'),
-]
-
